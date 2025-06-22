3b3dd6b0d221fb792de48013bdc038b6e5cd946e
Zuzia <zuzanna.majewskaa@wp.pl>
2025-06-22 14:55:12 +0200

    Tworzenie okna aplikacji z zakładkami

--- main.py
+++ main.py
@@ -3,3 +3,22 @@ from tkinter import *
 from tkinter import ttk, messagebox
 import notatnik
 import tkintermapview
+
+
+# ======= Listy danych ==========
+tereny = []
+punkty = []
+pracownicy = []
+
+root = tk.Tk()
+root.title("System zarządzania terenami zalewowymi i punktami monitoringu")
+root.geometry('1400x800')
+
+notebook = ttk.Notebook(root)
+frame_teren = Frame(notebook)
+frame_punkt = Frame(notebook)
+frame_pracownik = Frame(notebook)
+notebook.add(frame_teren, text="Tereny zalewowe")
+notebook.add(frame_punkt, text="Punkty monitoringu")
+notebook.add(frame_pracownik, text="Pracownicy")
+notebook.pack(expand=1, fill='both')
